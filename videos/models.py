ac3cefec150209e2de67fd0d48cdad9ed556aa17
JON <jonn.thinkrtc@gmail.com>
2019-01-15 09:49:42 -0500

    update video model files

--- videos/models.py
+++ videos/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class VideoTutorials(models.Model):
+    title = models.CharField(max_length=100)
+    summary = models.TextField(max_length=500)
+    thumbnail_image = models.ImageField(upload_to="thumbnail_images/")
+
+
+    def __str__(self):
+        return self.title
