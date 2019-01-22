c7b365b465efe9464b0a09aed2d7de5ff15e031e
JON <jonn.thinkrtc@gmail.com>
2019-01-21 19:13:34 -0500

    static files AWS integration

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,6 @@
-#!/bin/sh
+#!/bin/bash
 
-if [ "$DATABASE" = "postgres" ]
-then
-    echo "Waiting for postgres..."
-
-    while ! nc -z $SQL_HOST $SQL_PORT; do
-      sleep 0.1
-    done
-
-    echo "PostgreSQL started"
-fi
+echo "running"
 
 python manage.py flush --no-input
 python manage.py migrate
