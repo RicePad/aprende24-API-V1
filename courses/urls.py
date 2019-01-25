03792cf21b2b4ef1ed83cc33906255896fd68a65
JON <jonn.thinkrtc@gmail.com>
2019-01-25 09:38:01 -0500

    courses url id update

--- courses/urls.py
+++ courses/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 app_name = 'courses'
@@ -6,5 +6,6 @@ app_name = 'courses'
 urlpatterns = [
     path('', views.CourseListView.as_view(), name="course-list"),
     path('new/', views.CourseCreateView.as_view(), name="course-new"),
+    re_path(r'(?P<pk>\d+)$', views.CourseDetailView.as_view(), name="course-detail"),
     path('<slug>', views.CourseDetailView.as_view(), name="course-detail"),
 ]
