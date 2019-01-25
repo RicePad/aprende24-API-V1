4dda490b870d646383a251da1d11da017a78df83
JON <jonn.thinkrtc@gmail.com>
2019-01-25 12:03:24 -0500

    fix courses urls bug

--- courses/urls.py
+++ courses/urls.py
@@ -8,7 +8,6 @@ urlpatterns = [
     path('new/', views.CourseCreateView.as_view(), name="course-new"),
     re_path(r'^lesson/(?P<pk>\d+)$', views.LessonDetailView.as_view(), name="lesson-detail"),
     re_path(r'(?P<pk>\d+)$', views.CourseDetailView.as_view(), name="course-detail"),
-
     path('<slug>', views.CourseDetailView.as_view(), name="course-detail"),
     path('lessons/', views.LessonListView.as_view(), name="lesson-list"),
     path('lessons/new', views.LessonCreateView.as_view(), name="lesson-new"),
