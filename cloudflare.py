a790ca3ee348d983e5b19e27c9b556b3aaae05e3
JON <jonn.thinkrtc@gmail.com>
2019-01-31 22:46:05 -0500

    CloudflareAPI connection and GET list video functionality added

--- cloudflare.py
+++ cloudflare.py
@@ -1,18 +1,19 @@
-from django.views.generic import TemplateView
 import requests
 import json
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 
 
-
 cloudfare_headers = { 'Content-Type': 'application/json',
                       'X-Auth-Key' : CLOUDFARE_API,
-                      'X-Auth-Email': CLOUDFARE_USER ,}
+                      'X-Auth-Email': CLOUDFARE_USER ,
+}
 
 def get_cloudaflare_info():
 
     api_url = "{0}zones/{1}/media".format(CLOUDFARE_BASE_URL, CLOUDFARE_ZONE_ID)
+    # api_url = "{0}accounts?page=1&per_page=20&direction=desc".format(CLOUDFARE_BASE_URL)
+
 
     response = requests.get(api_url, headers=cloudfare_headers)
 
@@ -20,6 +21,8 @@ def get_cloudaflare_info():
         return json.loads(response.content.decode('utf-8'))
     else:
         return None
+
+cloudfare_account_info = get_cloudaflare_info()
 
 # if cloudfare_account_info is not None:
 #     # print("Here's your info: ")
@@ -29,20 +32,9 @@ def get_cloudaflare_info():
 # else:
 #     print('[!] Request Failed')
 
-# print(cloudfare_account_info)
-
 # jsonToDictionary = json.dumps(cloudfare_account_info)
 # print(jsonToDictionary)
-# print("type:", type(cloudfare_account_info))
 
-cloudflare_account_info = get_cloudaflare_info()
-
-def list_all_cloudfare_data(request):
-    cloudflare_info = cloudflare_account_info['result']
-    print("type:", type(cloudflare_info))
-    return render(request, "cloudflare_list.html", {"cloudflare_info": cloudflare_info})
-
-# Create your views here.
-
-class HomeView(TemplateView):
-    template_name = "home.html"
+def list_all_cloudfare_data(requests):
+    cloudfare_info = json.dumps(cloudfare_account_info)
+    return render(request, "cloudflare_list.html", {"cloudfare_info": cloudfare_info})
