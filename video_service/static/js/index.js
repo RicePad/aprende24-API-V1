90681a379a0b7031992ace199c05ee8de53722e9
JON <jonn.thinkrtc@gmail.com>
2019-03-04 20:19:26 -0500

    set up hot module css

--- video_service/static/js/index.js
+++ video_service/static/js/index.js
@@ -1,3 +1,5 @@
+import '../css/hello.css';
+
 const name = document.getElementById('name');
 const counter = document.getElementById('counter');
 let count = 0;
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,12 @@
 module.exports = {
     mode: 'development',
     entry: './video_service/static/js/index.js',
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader'],
+        },
+      ],
+    }
   }
